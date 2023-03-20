f5b4ab5ca4e8834b92c47e360c9cd8a1e101b355
=Ayushi Saxena <saxenaayu2106@gmail.com>
2023-03-20 20:35:29 +0530

    User Repository Added

--- src/main/java/co/springemail/EmailService/Repository/UserRepository.java
+++ src/main/java/co/springemail/EmailService/Repository/UserRepository.java
@@ -1,5 +1,16 @@
 package co.springemail.EmailService.Repository;
 
-public class UserRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+import org.springframework.stereotype.Repository;
+
+import co.springemail.EmailService.Domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepositoryImplementation<User, Integer> {
+
+	 List<User> findByUserRoleAndUserStatus(String userRole, int status);
+	  List<User> findByUserStatus(int status);
+	
 }
