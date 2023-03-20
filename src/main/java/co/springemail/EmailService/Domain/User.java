7da78080c39f563f43e6b77e5de25e0cdf209809
=Ayushi Saxena <saxenaayu2106@gmail.com>
2023-03-20 23:01:03 +0530

    Gettingsome connection errors

--- src/main/java/co/springemail/EmailService/Domain/User.java
+++ src/main/java/co/springemail/EmailService/Domain/User.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 
 
 @Entity
-@Table(name="emp")
+@Table(name="employee")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
